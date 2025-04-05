85285e32618ae2b183e10ece101ce91fef120df3
Crunch <57434983+Cr-nch@users.noreply.github.com>
2025-04-05 14:34:27 +1100

    Camera Movement

--- Ludum Dare 2025/Assets/CameraMovement.cs
+++ Ludum Dare 2025/Assets/CameraMovement.cs
@@ -7,6 +7,7 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private float Acceleration = 0.1f;
     [SerializeField] private float StartingSpeed = 3f;
     private float speed = 0;
+    [SerializeField] private Transform player;
 
     void Start()
     {
@@ -16,7 +17,14 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position += new Vector3(0,-speed*Time.fixedDeltaTime,0);
-        speed += Acceleration*Time.fixedDeltaTime;
+        speed = StartingSpeed+Acceleration*Mathf.Abs(transform.position.y);
+
+        float targetSpeed = speed;
+
+        Debug.Log(player.position.y-transform.position.y);
+        if(player.position.y-transform.position.y < -1f)
+            targetSpeed = speed+Mathf.Pow(player.position.y-transform.position.y,2);
+
+        transform.position += new Vector3(0,-targetSpeed*Time.fixedDeltaTime,0);
     }
 }
